40d2d8f9eefbc50fb6260fb55ce746addb517691
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2024-03-17 16:29:17 +0100

    🔧 Added Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-function App() {
-  return <h1>Historical Document Datasets</h1>;
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-center">
+      Historical Document Datasets
+    </h1>
+  );
 }
-
-export default App;
