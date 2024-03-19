ba6475a7d20991b2e46656ad28561092b1495696
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2024-03-19 21:33:42 +0100

    Added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
+import Home from "./pages/Home.tsx";
+import Layout from "./pages/Layout.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Historical Document Datasets</h1>;
+}
