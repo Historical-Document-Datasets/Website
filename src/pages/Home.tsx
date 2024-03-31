576e4741a055f31027e9d5fca88fa95433ab4ba1
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2024-03-31 18:58:27 +0200

    Added ephemeral home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { ExternalLink } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Historical Document Datasets</h1>;
+  return (
+    <div className="pt-6">
+      <h1 className="text-3xl text-center">
+        Historical Document Datasets — Preview
+      </h1>
+      <p className="my-4">
+        This will be the home page. This is still under construction.
+      </p>
+      <p className="mb-2">In the meanwile, you can still</p>
+      <Link to={"browse/"}>
+        <Button variant={"outline"}>
+          <ExternalLink className="mr-2 h-4 w-4" />
+          Browse preview
+        </Button>
+      </Link>
+    </div>
+  );
 }
